a540db3c4fd241a91017fe0357af8ce440e48f6b
qht <qht20050506@outlook.com>
2024-09-21 22:18:09 +0800

    吸顶导航的交互实现

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,10 +2,12 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFlexd from './components/LayoutFlexd.vue';
 </script>
 <template>
+    <LayoutFlexd />
     <LayoutNav />
     <LayoutHeader />
     <RouterView />
     <LayoutFooter />
-</template>
+</template> 
